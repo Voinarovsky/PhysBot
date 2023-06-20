8795eb9898e05a05d552922b02a321effbd9d12a
DjonChai <kirilldzis5@gmail.com>
2023-06-20 08:51:44 +0300

    lastcommit

--- PhysBot/main.py
+++ PhysBot/main.py
@@ -1,10 +1,3 @@
-import telebot
-from telebot import types
-
-bot = telebot.TeleBot('5864360081:AAFVfH3fXLHk2mo08pTMJGNIr4zsoOQntvo')
-
-@bot.message_handler(commands=['website'])
-def website(message):
-    marcup = types.InlineKeyboardMarkup()
-    marcup.add(types.InlineKeyboardButton('Сайт My.Itmo', url='https://my.itmo.ru'))
-    bot.send_message(message.chat.id, reply_markup=markup)
+import datetime
+nowdate = datetime.datetime.today().weekday()
+print((datetime.datetime.today().isocalendar().week + 1) % 2)
